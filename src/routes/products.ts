6fb7b920e2051191d22ea8260afb52114dc0516c
robert-virtual <robertocastill63@gmail.com>
2022-10-13 15:58:33 -0600

    typeos

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -9,7 +9,7 @@ prodsRouter.get("/", async (req, res) => {
   try {
     const { name } = req.query as { name: string };
     const data = await products.find({ name });
-    res.json({ msg: "get prodcuts", data });
+    res.json({ msg: "get products", data });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -17,13 +17,13 @@ prodsRouter.get("/", async (req, res) => {
 
 prodsRouter.get("/:id", (req, res) => {
   const id = req.params.id;
-  res.json({ msg: "get prodcuts" });
+  res.json({ msg: "get products" });
 });
 
 prodsRouter.post("/", async (req, res) => {
   try {
     const data = await products.create(req.body);
-    res.json({ msg: "post prodcuts", data });
+    res.json({ msg: "post products", data });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -33,7 +33,7 @@ prodsRouter.put("/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const data = await products.update(id, req.body);
-    res.json({ msg: "put prodcuts", data });
+    res.json({ msg: "put products", data });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -43,7 +43,7 @@ prodsRouter.delete("/:id", (req, res) => {
   try {
     const id = req.params.id;
     const data = products.delete(id);
-    res.json({ msg: "delete prodcuts",data });
+    res.json({ msg: "delete products",data });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
